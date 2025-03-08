07aecc967ca0cd60636a261469537c2c5a1333bd
samsarj <samsarj@gmail.com>
2025-03-08 12:14:15 +0000

    temp route for song editor testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Control from "./pages/Control";
 import LiveView from "./pages/LiveView"; // Import LiveView component
+import SongEditor from "./components/SongEditor"; // Import SongEditor component
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Control />} />
         <Route path="/live" element={<LiveView />} />
+        <Route path="/edit-song" element={<SongEditor />} />
       </Routes>
     </Router>
   )
